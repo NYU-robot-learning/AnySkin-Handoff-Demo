7eb2fc1cdc47eaa2510d9b805a9e87e515aad1c5
eneserciyes <m.eneserciyes@gmail.com>
2024-09-27 00:12:25 -0400

    CORL Demo

---
+++ robot/controller.py
@@ -0,0 +1,321 @@
+from collections import deque
+import logging
+import time
+
+import torch
+import gradio as gr
+import numpy as np
+
+from zmq_utils import create_request_socket, ZMQKeypointPublisher
+from reskin_server import ReskinSensorSubscriber
+
+logger = logging.getLogger(__name__)
+STRETCH_GRIPPER_MAX = 40
+
+LOCALHOST = "127.0.0.1"
+ANYCAST = "0.0.0.0"
+
+def get_home_param(
+    h=0.7,
+    y=0.02,
+    x=0.0,
+    yaw=0.0,
+    pitch=0.0,
+    roll=0.0,
+    gripper=1.0,
+    closing_threshold=0.5,
+    reopening_threshold=0.5,
+    stretch_gripper_max=None,
+    stretch_gripper_min=None,
+    stretch_gripper_tight=None,
+    sticky_gripper=None,
+    # Below the first value, it will close, above the second value it will open
+    gripper_threshold_post_grasp_list=None,
+):
+    """
+    Returns a list of home parameters
+    """
+    return [
+        h,
+        y,
+        x,
+        yaw,
+        pitch,
+        roll,
+        gripper,
+        stretch_gripper_max,
+        stretch_gripper_min,
+        stretch_gripper_tight,
+        sticky_gripper,
+        closing_threshold,
+        reopening_threshold,
+        gripper_threshold_post_grasp_list,
+    ]
+
+def get_input_tensor_sequence(sensor_queue, downsample, device):
+    X = np.array(sensor_queue)
+    X = np.diff(X[::downsample], axis=0)
+    X = torch.as_tensor(X, dtype=torch.float32, device=torch.device(device))
+    return X
+
+class Controller:
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.task = cfg["task"]
+        network_cfg = cfg["network"]
+
+        publisher = ZMQKeypointPublisher(
+            network_cfg.get("host", ANYCAST), network_cfg["action_port"]
+        )
+        subscriber = ReskinSensorSubscriber() # TODO: take this in the slip-deteciton-demo repo
+
+        self.flag_socket = create_request_socket(
+            network_cfg.get("remote", LOCALHOST), port=network_cfg["flag_port"]
+        )
+
+        self.publisher = publisher
+        self.subscriber = subscriber
+
+        self.sensor_queue = deque(maxlen=cfg["tactile_buffer_size"])
+        self.prediction_queue = deque(maxlen=cfg["prediction_buffer_size"])
+
+        self.device = cfg["device"]
+
+        self.run_n = -1
+        self.step_n = 0
+        self.h = cfg["robot_params"]["h"]
+
+        self.abs_gripper = cfg["robot_params"]["abs_gripper"]
+        self.gripper = 1.0
+        self.rot_unit = cfg["robot_params"]["rot_unit"]
+        self.slip_detection_freq = int(100 / cfg["slip_detection_freq"])
+
+        self._max_gripper = 1.0 # TODO: find a suitable value for this
+
+        self.demo = self._init_demo()
+
+    def get_gripper_val(self):
+        return self.gripper
+
+    def setup_model(self, model):
+        self.model = model
+        self.model.to(self.device)
+        self.model.eval()
+
+    def reset_experiment(self):
+        self.run_n += 1
+        self.step_n = 0
+        self.gripper = 1.0
+        self.model.reset()
+
+    def _run(self):
+        logger.info("Run robot handover")
+
+        policy_counter = self.slip_detection_freq
+        while True:
+            start_time = time.time()
+            anyskin_state = self.subscriber.get_sensor_state()
+            self.sensor_queue.append(anyskin_state["sensor_values"])
+            policy_counter -= 1
+
+            if policy_counter <= 0 and len(self.sensor_queue) == self.cfg["tactile_buffer_size"]:
+                policy_counter = self.slip_detection_freq # reset counter
+                with torch.no_grad():
+                    X = get_input_tensor_sequence(
+                        self.sensor_queue,
+                        self.cfg["downsample"],
+                        self.device,
+                    )
+                    Yhat = self.model.step(X.unsqueeze(0))
+
+                    if Yhat:
+                        logger.info("Slip")
+                        self.prediction_queue.append(1)
+                    else:
+                        logger.info("No-slip")
+                        self.prediction_queue.append(0)
+
+                if sum(self.prediction_queue) / self.cfg["prediction_buffer_size"] > self.cfg["slip_detection_threshold"]:
+                    logger.info("Opening gripper")
+                    self._open_gripper()
+                    # clear the prediction queue
+                    self.prediction_queue.clear()
+                    self.sensor_queue.clear()
+                    break
+
+
+            elapsed_time = time.time() - start_time
+            sleep_time = max(0, 0.01 - elapsed_time) # run at 100Hz
+            time.sleep(sleep_time)
+
+
+    def _open_gripper(self):
+        logger.info("Opening gripper")
+        self.gripper = 1.0
+        self.flag_socket.send(b"")
+        self.publisher.pub_keypoints([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, self.gripper], "robot_action")
+        self.flag_socket.recv()
+        time.sleep(0.5)
+
+    def _close_gripper(self):
+        logger.info("Close gripper")
+        self.gripper = 0.0
+        self.flag_socket.send(b"")
+        self.publisher.pub_keypoints([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, self.gripper], "robot_action")
+        self.flag_socket.recv()
+        time.sleep(0.5)
+
+    def _run_home(self):
+        logger.info("Publishing home")
+        self.flag_socket.send(b"")
+        self.publisher.pub_keypoints([1], "home")
+        self.reset_experiment()
+        self.flag_socket.recv()
+
+    def update_robot_params(
+        self,
+        height,
+        gripper,
+        # gripper_close_threshold,
+        # gripper_open_threshold,
+        gripper_tight,
+    ):
+        logger.info(
+            f"Publishing params: height={height}, gripper={gripper}, gripper_tight={gripper_tight}"
+        )
+
+        # Map starting position back to the height and base position, then publish that location to move
+        x: int = 0
+        self.h = height
+
+        self.flag_socket.send(b"")
+        self.publisher.pub_keypoints(
+            # Below the first value, it will close, above the second value it will open
+            get_home_param(
+                h=height,
+                x=x,
+                stretch_gripper_max=STRETCH_GRIPPER_MAX * gripper,
+                closing_threshold=0.5,
+                reopening_threshold=0.5,
+                stretch_gripper_tight=STRETCH_GRIPPER_MAX * gripper_tight,
+            ),
+            "params",
+        )
+        self.flag_socket.recv()
+
+
+    def _init_demo(self):
+        # Convert the above demo to blocks.
+        with gr.Blocks(theme="soft", analytics_enabled=False) as demo:
+            gr.Markdown(
+                """
+            # AnySkin Demo Admin Tool
+            1. Press "Close Gripper" to grasp an object.
+            2. Then, click "Start Handover".
+            3. Done!
+            """
+            )
+            with gr.Row():
+                with gr.Column(scale=1):
+                    gripper_val = gr.Number(
+                        label="Gripper value",
+                        show_label=True,
+                        interactive=False,
+                        value=self.get_gripper_val,
+                        every=gr.Timer(0.03),
+                    )
+
+                with gr.Column(scale=4):
+                    height = gr.Slider(0, 1, value=0.7, step=0.01, label="Height")
+                    gripper = gr.Slider(
+                        0,
+                        1,
+                        value=self._max_gripper,
+                        step=0.01,
+                        label="Gripper max width",
+                        info="0.33 for door opening, 0.5 for drawer opening, 1 for everything else.",
+                    )
+                    gripper_tight = gr.Slider(
+                        -0.5,
+                        0.2,
+                        value=-0.25,
+                        step=0.01,
+                        label="Gripper tightness",
+                        info="This is how tight the gripper becomes when it closes. The lower the value, the tighter the gripper.",
+                    )
+
+                    with gr.Row():
+                        update_params_button = gr.Button(
+                            "Update params", variant="secondary"
+                        )
+                        update_params_button.click(
+                            fn=self.update_robot_params,
+                            inputs=[
+                                height,
+                                gripper,
+                                gripper_tight,
+                            ],
+                            outputs=None,
+                        )
+                        home_button = gr.Button("Home the robot")
+                        home_button.click(fn=self._run_home)
+
+                    with gr.Row():
+                        open_button = gr.Button("Open Gripper")
+                        open_button.click(fn=self._open_gripper)
+
+                        close_button = gr.Button("Close Gripper")
+                        close_button.click(fn=self._close_gripper)
+
+                    step_button = gr.Button("Start handover", variant="primary")
+                    step_button.click(fn=self._run)
+
+        return demo
+
+    def run(self):
+        self.flag_socket.send(b"")
+        time.sleep(0.5)
+        self.publisher.pub_keypoints(get_home_param(h=self.h), "params")
+        self.flag_socket.recv()
+
+        print("Launched the Gradio UI at http://ROBOT_IP:7860/")
+        self.demo.launch(prevent_thread_lock=False, server_name="0.0.0.0", quiet=True)
+
+        # TODO: maybe an instruction processing loop here
+        # while True:
+        #     # self.flag_socket.send(b"")
+
+        #     # wait for instruction
+        #     instruction = input("Enter instruction: ")
+        #     start_time = time.time()
+
+        #     if instruction.lower() == "q":
+        #         instruction = self._process_instruction(instruction)
+        #         break
+        #     elif instruction.lower() == "rc":
+        #         self._run()
+        #         self.flag_socket.recv()
+        #         instruction = ""
+        #         while len(instruction) == 0:
+        #             instruction = self.run_continous()
+        #         continue
+
+        #     # process and send instruction to robot
+        #     instruction = self._process_instruction(instruction)
+
+        #     # continue loop only once instruction has been executed on robot
+        #     self.flag_socket.recv()
+
+        #     elapsed_time = time.time() - start_time
+        #     sleep_time = max(0, 0.1 - elapsed_time)
+        #     time.sleep(sleep_time)
+
+
+
+
+
+
+
+
+
+
---
+++ run.py
@@ -0,0 +1,77 @@
+import os
+
+import torch
+from torch import nn
+import hydra
+from omegaconf import OmegaConf
+import gdown
+
+from robot.controller import Controller
+
+TASK_GDRIVE_ID = {
+    "handover": "" #TODO: upload the model to gdrive id for this
+}
+
+class WrapperPolicy(nn.Module):
+    def __init__(self, model, input_size):
+        super().__init__()
+        self.model = model
+        self.input_size = input_size
+
+    def step(self, data, *args, **kwargs):
+        model_out = self.model(data[..., :self.input_size])
+        Yhat = torch.sigmoid(model_out)
+        return Yhat > 0.5 # gripper open/close
+
+    def reset(self):
+        pass
+
+def get_model_weight_pth(task_name):
+    if task_name not in TASK_GDRIVE_ID:
+        raise ValueError(f"Task \'{task_name}\' is invalid. Please choose from {TASK_GDRIVE_ID.keys()}.")
+    task_dir = f"checkpoints/{task_name}"
+    if not os.path.exists(f"{task_dir}/checkpoint.pt"):
+        os.makedirs(task_dir)
+        gdrive_id = TASK_GDRIVE_ID[task_name]
+        url = f'https://drive.google.com/uc?id={gdrive_id}'
+        output_path = f"{task_dir}/checkpoint.pt"
+        gdown.download(url, output_path, quiet=False)
+
+    return f"{task_dir}/checkpoint.pt"
+
+def init_model(cfg: OmegaConf):
+    model = hydra.utils.instantiate(cfg.model)
+    model = model.to(cfg.device)
+
+    model_weight_pth = cfg.get("model_weight_pth")
+
+    if model_weight_pth is None:
+        model_weight_pth = get_model_weight_pth(task_name=cfg.task)
+
+    checkpoint = torch.load(model_weight_pth, weights_only=True, map_location=cfg.device)
+    model.load_state_dict(checkpoint)
+
+    model_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    # print model params in millions with %.2f
+    print(f"Model parameters: {model_parameters / 1e6:.2f}M")
+    policy = WrapperPolicy(model, input_size=cfg.model.input_size)
+
+    return policy
+
+
+def run(cfg: OmegaConf):
+    model = init_model(cfg)
+    dict_cfg = OmegaConf.to_container(cfg, resolve=True)
+
+    controller = Controller(cfg=dict_cfg)
+    controller.setup_model(model)
+    controller.run()
+
+
+@hydra.main(version_base="1.2", config_name="run_handover_demo", config_path="configs")
+def main(cfg: OmegaConf):
+    run(cfg)
+
+
+if __name__ == "__main__":
+    main()
